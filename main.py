b0b5fec6ca1663c5d8abf46d6ad0839b323eff75
AfinnityRae <29024047+CoolDog22YT@users.noreply.github.com>
2023-02-26 22:46:10 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,35 @@
+name=input("whats your name")
+# this function means that what is inputed in this question will be collected and checked
+if name == "Bus":
+  print("ha ha")
+  #This shows that if the name is bus then it will ignore what is below and just print "ha ha"
+else:
+  print ("dumb name")
+  #As explained before if the name is not bus then it will say dumb name
+x = lambda y: y+2
+#taken from a video lambda is a function
+my_list = ("apple,cookie,chocolate,hide")
+#list can do many thing but this is a variabl 
+print (my_list)
+
+print(1==2)
+#last output says false because 1 does not equal 2 
+print(4==4)
+#This is shown as true because 4 is equal to itself
+a=1
+b=1
+print(a==b)
+#this is show to be true because i have defined the variables 
+C=200
+E=100
+Z=300
+C+E==Z
+if (C+E==Z): 
+  print("yeah math")
+  b
+  # Program to generate a random number between 0 and 9
+
+# importing the random module
+import random
+
+print(random.randint(0,9))
